2ba03a272d0b939d599cf1745bf79592ffb799e6
vexplay-lcc <DanSul32@outlook.com>
2024-12-01 18:11:10 +0000

    test

---
+++ login test/config.js
@@ -0,0 +1,14 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAVcaccF7p0LRirkEFIoLVjvXOywba6gNg",
+    authDomain: "vexplay-56fef.firebaseapp.com",
+    projectId: "vexplay-56fef",
+    storageBucket: "vexplay-56fef.firebasestorage.app",
+    messagingSenderId: "444717290098",
+    appId: "1:444717290098:web:6fc74f7ed0fa97e23968eb",
+    measurementId: "G-31PKLWRJ4G"
+};
+export {firebaseConfig};
+
+
+
+
